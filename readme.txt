75ff1d934b671306fa4227a32b849c86fb6dd295
mitch.morris <mitch.morris@192.168.195.177>
2023-05-19 11:12:51 -0400

    Added ability to select an output format.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 Converts delimited file from Mobius View and converts it to a standard format. 
-Run convert.cmd and pass a path and filename for the input file. Optionally pass a 2nd argument of true to delete the original input file.
+Run convert.cmd and pass a path and filename for the input file. 
+    Optionally pass a 2nd argument of true to delete the original input file.
+    Optionally pass a 3rd argument of one of the keywords listed below to specify an output format.
 If you do not pass true, the original filename gets an extension of .original. 
+
--- src/main/java/com/mitch/csvconvert/Convert.java
+++ src/main/java/com/mitch/csvconvert/Convert.java
@@ -14,21 +14,61 @@ import org.apache.commons.csv.CSVRecord;
 
 /**
  *
- * Convert an unknown csv format into a known csv format
+ * Convert an unknown csv format into a known csv format todo - command line
+ * parameters
  */
 public class Convert {
 
     public static void main(String[] args) {
+        CSVFormat reqOutputFormat = CSVFormat.DEFAULT;
         boolean delfile = false;
+        if (args.length < 1) {
+            System.err.println("No parameters present. Must pass a filename.");
+            System.exit(1);
+        }
         if (args.length > 1) {
             String sdelfile = args[1];
             if (sdelfile.equalsIgnoreCase("true")) {
                 delfile = true;
             }
         }
-        if (args.length < 1) {
-            System.err.println("No parameters present. Must pass a filename.");
-            System.exit(1);
+        if (args.length > 2) {
+            String outfmt = args[2].toUpperCase();
+            switch (outfmt) {
+                case "DEFAULT":
+                    reqOutputFormat = CSVFormat.DEFAULT;
+                    break;
+                case "EXCEL":
+                    reqOutputFormat = CSVFormat.EXCEL;
+                    break;
+                case "INFORMIX_UNLOAD":
+                    reqOutputFormat = CSVFormat.INFORMIX_UNLOAD;
+                    break;
+                case "INFORMIX_UNLOAD_CSV":
+                    reqOutputFormat = CSVFormat.INFORMIX_UNLOAD_CSV;
+                    break;
+                case "MYSQL":
+                    reqOutputFormat = CSVFormat.MYSQL;
+                    break;
+                case "RFC4180":
+                    reqOutputFormat = CSVFormat.RFC4180;
+                    break;
+                case "ORACLE":
+                    reqOutputFormat = CSVFormat.ORACLE;
+                    break;
+                case "POSTGRESQL_CSV":
+                    reqOutputFormat = CSVFormat.POSTGRESQL_CSV;
+                    break;
+                case "POSTGRESQL_TEXT":
+                    reqOutputFormat = CSVFormat.POSTGRESQL_TEXT;
+                    break;
+                case "POSTGRESQL_TDF":
+                    reqOutputFormat = CSVFormat.TDF;
+                    break;
+                default:
+                    System.err.println("Requested format not valid: " + outfmt);
+                    System.exit(1);
+            }
         }
         File f = new File(args[0]);
         if (!f.canRead()) {
@@ -46,7 +86,7 @@ public class Convert {
         try {
             reader = new BufferedReader(new FileReader(f.getAbsolutePath()));
             BufferedWriter csvWriter = new BufferedWriter(new FileWriter(args[0]));
-            CSVPrinter csvPrinter = new CSVPrinter(csvWriter, CSVFormat.RFC4180);
+            CSVPrinter csvPrinter = new CSVPrinter(csvWriter, reqOutputFormat);
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
             for (CSVRecord record : csvParser) {
                 for (String field : record) {
